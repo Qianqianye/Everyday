69c51b1e08af66898e78bd9f907e733c612b69fe
Qianqianye <qianqian.ye44@gmail.com>
2016-09-19 23:07:53 -0700

    Updated pressButton function

--- Day8/js/sketch.js
+++ Day8/js/sketch.js
@@ -1,14 +1,36 @@
             var VELOCITY = 2;
-            var PARTICLES = 200;
+            var PARTICLES = 300;
             
             var mouse = {x:0, y:0};
-            var particles = [];
-            // var colors = [ "#000000","#FF0000","#FFFF00" ];
-            // var colors = [ "#b3ecec","#89ecda","#43e8d8","#40e0d0","#3bd6c6" ];  //Shades of Turquoise
-            // var colors = [ "#b3ecec","#89ecda","#3bd6c6" ];  
-             var colors = [ "#f9cbcb","#f8ffe5","#caeadc","#baab8c","#765d5d" ];   //Shades of Icecream
-            // var colors = [ "#7465b4","#4b5289","#504d72","#716c83","#8e8ca5" ];   //Shades of Sadness
-             // var colors = [ "#e39bf3","#d767cf","#9968d1","#6138bb","#0990b3" ];   //Shades of Neon
+            var particles = []; 
+            var colors = [ "#f9cbcb","#f8ffe5","#caeadc","#baab8c","#765d5d" ];   //Shades of Icecream
+
+            function pressButton(buttonName){
+
+                if ('icecream'.indexOf(buttonName) != -1 ) {
+                    colors = [ "#f9cbcb","#f8ffe5","#caeadc","#baab8c","#765d5d" ];   //Shades of Icecream
+                    console.log('lets eat icecream');
+                }
+                else if('calm'.indexOf(buttonName) != -1 ) {
+                    colors = [ "#7465b4","#4b5289","#504d72","#716c83","#8e8ca5" ];   //Shades of Sadness
+                    console.log('calm now');
+                }
+                else if('active'.indexOf(buttonName) != -1 ) {
+                    colors = [ "#b3ecec","#89ecda","#43e8d8","#40e0d0","#3bd6c6" ];  //Shades of Turquoise
+                    console.log('active time!');
+                }
+                else if('party'.indexOf(buttonName) != -1 ) {
+                    colors = [ "#e39bf3","#d767cf","#9968d1","#6138bb","#0990b3" ];   //Shades of Neon
+                console.log('PARTY time!!!');
+                }else{
+                    console.log('that button is not programmed yet');
+                }
+
+                for( var i = 0; i < PARTICLES; i++ ) {
+                    particles[i].color = colors[ Math.floor( Math.random() * colors.length ) ];             
+                }
+            }
+
             var canvas = document.getElementById('projector');
             var context;
             
@@ -16,13 +38,13 @@
                 context = canvas.getContext('2d');
                 
                 for( var i = 0; i < PARTICLES; i++ ) {
-                    particles.push( { 
+                        particles.push( { 
                         x: Math.random()*window.innerWidth, 
                         y: Math.random()*window.innerHeight, 
                         vx: ((Math.random()*(VELOCITY*2))-VELOCITY),
                         vy: ((Math.random()*(VELOCITY*2))-VELOCITY),
                         size: 1+Math.random()*3,
-                        color: colors[ Math.floor( Math.random() * colors.length ) ]
+                        color: colors[ Math.floor( Math.random() * colors.length ) ] 
                     } );
                 }
                 
@@ -78,7 +100,7 @@
                         particle.currentSize = particle.size*distanceFactor;
                     }
                     
-                    context.fillStyle = particle.color;
+                    context.fillStyle = particle.color; ///------------------------!color------------------------//////
                     context.beginPath();
                     context.arc(particle.x,particle.y,particle.currentSize,0,Math.PI*2,true);
                     context.closePath();
